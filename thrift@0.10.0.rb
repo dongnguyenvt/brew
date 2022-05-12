4118427aa38e3402032b9d581c3562c6491f18eb
Dong Nguyen <dongnguyen@axon.com>
2022-05-12 15:36:41 +0700

    try fix build

--- thrift@0.10.0.rb
+++ thrift@0.10.0.rb
@@ -1,64 +1,76 @@
 class ThriftAT0100 < Formula
-    desc "Framework for scalable cross-language services development"
-    homepage "https://thrift.apache.org/"
-    url "https://archive.apache.org/dist/thrift/0.10.0/thrift-0.10.0.tar.gz"
-    sha256 "2289d02de6e8db04cbbabb921aeb62bfe3098c4c83f36eec6c31194301efa10b"
+  desc "Framework for scalable cross-language services development"
+  homepage "https://thrift.apache.org/"
+  url "https://archive.apache.org/dist/thrift/0.10.0/thrift-0.10.0.tar.gz"
+  sha256 "2289d02de6e8db04cbbabb921aeb62bfe3098c4c83f36eec6c31194301efa10b"
 
-    head do
-      url "https://git-wip-us.apache.org/repos/asf/thrift.git"
-  
-      depends_on "autoconf" => :build
-      depends_on "automake" => :build
-      depends_on "libtool" => :build
-      depends_on "pkg-config" => :build
-    end
-  
-    option "with-haskell", "Install Haskell binding"
-    option "with-erlang", "Install Erlang binding"
-    option "with-java", "Install Java binding"
-    option "with-perl", "Install Perl binding"
-    option "with-php", "Install PHP binding"
-    option "with-libevent", "Install nonblocking server libraries"
-  
-    depends_on "bison" => :build
-    depends_on "boost"
-    depends_on "openssl"
-    depends_on "libevent" => :optional
-    depends_on "python@2" => :optional
-  
-    def install
-      system "./bootstrap.sh" unless build.stable?
-  
-      exclusions = ["--without-ruby", "--disable-tests", "--without-php_extension"]
-  
-      exclusions << "--without-python" if build.without? "python@2"
-      exclusions << "--without-haskell" if build.without? "haskell"
-      exclusions << "--without-java" if build.without? "java"
-      exclusions << "--without-perl" if build.without? "perl"
-      exclusions << "--without-php" if build.without? "php"
-      exclusions << "--without-erlang" if build.without? "erlang"
-  
-      ENV.cxx11 if MacOS.version >= :mavericks && ENV.compiler == :clang
-  
-      # Don't install extensions to /usr:
-      ENV["PY_PREFIX"] = prefix
-      ENV["PHP_PREFIX"] = prefix
-  
-      system "./configure", "--disable-debug",
-                            "--prefix=#{prefix}",
-                            "--libdir=#{lib}",
-                            "--with-openssl=#{Formula["openssl"].opt_prefix}",
-                            *exclusions
-      ENV.deparallelize
-      system "make", "-j8"
-      system "make", "install"
-    end
-  
-    def caveats; <<~EOS
-      To install Ruby binding:
-        gem install thrift
-      To install PHP extension for e.g. PHP 5.5:
-        brew install homebrew/php/php55-thrift
-    EOS
-    end
+  head do
+    url "https://github.com/apache/thrift.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "pkg-config" => :build
   end
+
+  option "with-java", "Install Java binding"
+
+  deprecated_option "with-python" => "with-python@2"
+
+  depends_on "bison" => :build
+  depends_on "boost"
+  depends_on "openssl"
+  depends_on "python@2" => :optional
+
+  if build.with? "java"
+    depends_on "ant" => :build
+    depends_on :java => "1.8"
+  end
+
+  def install
+    system "./bootstrap.sh" unless build.stable?
+
+    args = %W[
+      --disable-debug
+      --disable-tests
+      --prefix=#{prefix}
+      --libdir=#{lib}
+      --with-openssl=#{Formula["openssl"].opt_prefix}
+      --without-erlang
+      --without-haskell
+      --without-perl
+      --without-php
+      --without-php_extension
+      --without-ruby
+    ]
+
+    args << "--without-python" if build.without? "python@2"
+    args << "--without-java" if build.without? "java"
+
+    ENV.cxx11 if MacOS.version >= :mavericks && ENV.compiler == :clang
+
+    # Don't install extensions to /usr:
+    ENV["PY_PREFIX"] = prefix
+    ENV["PHP_PREFIX"] = prefix
+    ENV["JAVA_PREFIX"] = buildpath
+
+    system "./configure", *args
+    ENV.deparallelize
+    system "make", "-j8"
+    system "make", "install"
+
+    # Even when given a prefix to use it creates /usr/local/lib inside
+    # that dir & places the jars there, so we need to work around that.
+    (pkgshare/"java").install Dir["usr/local/lib/*.jar"] if build.with? "java"
+  end
+
+  def caveats; <<~EOS
+    To install Ruby binding:
+      gem install thrift
+  EOS
+  end
+
+  test do
+    system "#{bin}/thrift", "--version"
+  end
+end
